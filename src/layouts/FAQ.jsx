4bd9123fd360592e18f475bcdaa2b300a70a36e2
ReisVC <vitor.reisvc@gmail.com>
2025-07-30 14:34:36 -0300

    adiciona faq section

--- src/layouts/FAQ.jsx
+++ src/layouts/FAQ.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
 import './FAQ.css'
+import Accordion from '../components/Accordion';
 
 function FAQ() {
   return (
-    <section>
-        
+    <section className='section-faq'>
+      <h3 className='title-faq'>Frequently Asked Questions</h3>
+      <p className='subtitle-faq'>From quick answers to in-depth guidance, we’re committed to making sure you feel confident</p>
+
+      <Accordion title="What services does your travel agency offer?"
+        children="We offer a wide range of travel services, including flight bookings, hotel reservations, holiday packages, customized itineraries, visa assistance, travel insurance, and tour arrangements for both domestic and international destinations." />
+      <Accordion title="Can I customize my travel package?"
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+      <Accordion title="Do you offer travel insurance?"
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+      <Accordion title="What happens if my flight is delayed or canceled?"
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+      <Accordion title="How do I make a booking?"
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+      <Accordion title="Do I need to pay in full at the time of booking?"
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+      <Accordion title="Can I cancel or reschedule my trip?"
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
     </section>
-  )
+  );
 }
 
 export default FAQ
